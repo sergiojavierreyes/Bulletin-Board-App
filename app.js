10f3272a58ec378da54171a46bae21d849a7e791
Sergio Reyes <sergio.javier.reyes@me.com>
2016-11-02 12:27:31 +0100

    Created a table within the database

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-//require postgres library
+//require postgres library and necessary modules
 const pg = require ('pg')
+const express = require('express')
+const fs= require('fs')
+const app = express()
 
+
+//connects to the database
+let connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletinboard';
+
+app.set('view engine', 'pug')
+app.set('views', __dirname + "/views")
+
+
+//Renders a page that displays a form 
+app.get('/form', (req,res)=> {
+	res.render('form')
+})
+
+
+
+
+//Created a server that listens on port 1337
+app.listen(1337,() => {
+	console.log('Up and Running at port 1337!')
+})
 
