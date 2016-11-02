00e879ad86608848c66669d358e75387fef10e6e
Sergio Reyes <sergio.javier.reyes@me.com>
2016-11-02 14:56:29 +0100

    Adds post request to the database

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const pg = require ('pg')
 const express = require('express')
 const fs= require('fs')
 const app = express()
+const bodyParser = require ('body-parser')
+
+app.set('view engine', 'pug')
+app.set('views', __dirname + "/views")
+
+app.use(bodyParser.urlencoded({extended:true}))
 
 
 //connects to the database
 let connectionString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletinboard';
 
-app.set('view engine', 'pug')
-app.set('views', __dirname + "/views")
 
 
 //Renders a page that displays a form 
@@ -18,6 +22,25 @@ app.get('/form', (req,res)=> {
 })
 
 
+//Fetches messages from database
+app.get('/results', (req, res) =>{
+	
+	res.redirect('results')
+
+})
+
+//takes in the post request from the form, and shows each of the messages people have posted.
+app.post('/results', (req, res) => {
+	//pg connect
+	pg.connect(connectionString, (err, client, done) =>{
+		if (err) throw (err)
+			client.query('insert into messages(title, body) values ($1, $2)', [
+				req.body.title, req.body.textmessage])
+
+		done()
+		res.redirect('results') 
+	})
+})
 
 
 //Created a server that listens on port 1337
